244f98512ff8cdaafd707d48478a0992d5bafb9a
eliefly <eliefly@163.com>
2019-07-26 18:07:07 +0800

    update

--- spring-boot-02-config/src/main/java/com/atguigu/springboot/bean/Person.java
+++ spring-boot-02-config/src/main/java/com/atguigu/springboot/bean/Person.java
@@ -1,6 +1,12 @@
 package com.atguigu.springboot.bean;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Email;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -27,6 +33,12 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "person")
 //@Validated
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain=true)
+@Builder
+@Data
 public class Person {
 
     /**
@@ -50,72 +62,72 @@ public class Person {
     private List<Object> lists;
     private Dog dog;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", boss=" + boss +
-                ", birth=" + birth +
-                ", maps=" + maps +
-                ", lists=" + lists +
-                ", dog=" + dog +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Person{" +
+//                "lastName='" + lastName + '\'' +
+//                ", age=" + age +
+//                ", boss=" + boss +
+//                ", birth=" + birth +
+//                ", maps=" + maps +
+//                ", lists=" + lists +
+//                ", dog=" + dog +
+//                '}';
+//    }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Boolean getBoss() {
-        return boss;
-    }
-
-    public void setBoss(Boolean boss) {
-        this.boss = boss;
-    }
-
-    public Date getBirth() {
-        return birth;
-    }
-
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
-
-    public Map<String, Object> getMaps() {
-        return maps;
-    }
-
-    public void setMaps(Map<String, Object> maps) {
-        this.maps = maps;
-    }
-
-    public List<Object> getLists() {
-        return lists;
-    }
-
-    public void setLists(List<Object> lists) {
-        this.lists = lists;
-    }
-
-    public Dog getDog() {
-        return dog;
-    }
-
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    public Boolean getBoss() {
+//        return boss;
+//    }
+//
+//    public void setBoss(Boolean boss) {
+//        this.boss = boss;
+//    }
+//
+//    public Date getBirth() {
+//        return birth;
+//    }
+//
+//    public void setBirth(Date birth) {
+//        this.birth = birth;
+//    }
+//
+//    public Map<String, Object> getMaps() {
+//        return maps;
+//    }
+//
+//    public void setMaps(Map<String, Object> maps) {
+//        this.maps = maps;
+//    }
+//
+//    public List<Object> getLists() {
+//        return lists;
+//    }
+//
+//    public void setLists(List<Object> lists) {
+//        this.lists = lists;
+//    }
+//
+//    public Dog getDog() {
+//        return dog;
+//    }
+//
+//    public void setDog(Dog dog) {
+//        this.dog = dog;
+//    }
 }
